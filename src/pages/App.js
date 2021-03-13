ce838d89f576378d5029676f788cda8fd26a23ec
freconur <freconur@gmail.com>
2021-03-13 17:57:31 -0500

    se modifico los handleChange para la pagina app y cambio de datos en tiempo ral

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,17 +3,37 @@ import Badge from '../components/Badge';
 import BadgesForm from '../components/BadgesForm';
 import './styles/App.css'
 class App extends React.Component {
+	state= { form: {
+		firstName:'',
+		lastName:'',
+		email:'',
+	}}
+
+	handleChange = e => {
+		this.setState ({
+			//queremos que el form tenga la informacion del evento que acaba de ocurrir
+			form: {
+				...this.state.form,
+				[e.target.name]: e.target.value,
+			}
+		})
+	}
     render() {
         return (
             <div className="container">
                 <div className="Badge__container">
                     <Badge 
-                    firstName="cachito"
-										lastName="mondongito"
-										email="freconur@gmail.com"
+                    firstName={this.state.form.firstName } //|| Name
+										lastName={this.state.form.lastName } //|| Last Name
+										email={this.state.form.email } //|| email
                     />
                     </div>
-                <div className="BadgeForm__container"><BadgesForm /></div>
+                <div className="BadgeForm__container">
+									<BadgesForm 
+									onChange={this.handleChange}
+									formValues={this.state.form}
+									/>
+									</div>
             </div>
         )
     }
