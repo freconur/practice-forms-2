b5edf7c77abeabe2c53d0c4e1cbb01a055355ee3
freconur <freconur@gmail.com>
2021-03-13 19:47:40 -0500

    practicamos por primera vez los ejecicios del form de la pagina

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,10 +4,10 @@ class Badge extends React.Component {
     render() {
         return (
 					<div>
-						<h1>Franco <br /> Condori</h1>
+						<h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
 						<p>Fronted Enginner</p>
 						<p>@freconur</p>
-						<p>freconur@gmail.com</p>
+						<p>{this.props.email}</p>
 					</div>
         )
     }
--- src/components/BadgesForm.js
+++ src/components/BadgesForm.js
@@ -1,6 +1,13 @@
 import React from 'react' ;
 
 class BadgesForm extends  React.Component {
+		/* state={}
+
+		handleChange = e => {
+			this.setState({
+				[e.target.name]: e.target.value,
+			})
+		} */
     render() {
 			return (
 				<div>
@@ -9,19 +16,28 @@ class BadgesForm extends  React.Component {
 								<label>First Name</label> <br />
 								<input 
 								type="text" 
-								name="fisrtName"></input>
+								name="firstName"
+								onChange={this.props.onChange}
+								value={this.props.formValues.firsName}
+								/>
 								</div>
 							<div>
 								<label>Last Name</label><br />
 								<input 
+								onChange={this.props.onChange}
 								type="text" 
-								name="lastName"></input>
+								name="lastName"
+								value={this.props.formValues.lastName}
+								/>
 							</div>
 							<div>
 								<label>Email</label><br />
 								<input 
+								onChange={this.props.onChange}
 								type="email" 
-								name="email"></input>
+								name="email"
+								value={this.props.formValues.email}
+								/>
 							</div>
 						</form>
 				</div>
