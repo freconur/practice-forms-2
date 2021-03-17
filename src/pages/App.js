8872980b70ebf0820644fd1ea807aa186211e32e
freconur <freconur@gmail.com>
2021-03-16 19:04:50 -0500

    practicando nuevamente por 3ra vez

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,13 +3,39 @@ import Badge from '../components/Badge';
 import BadgesForm from '../components/BadgesForm';
 import './styles/App.css'
 class App extends React.Component {
+  state = { form: {
+   /*  firstName:"nombres",
+    lastName:'apellidos',
+    email:'correo' */
+  }}
+
+  handleChange = e => {
+    this.setState({
+      form : {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
     render() {
-        return (
-            <div className="container">
-                <div className="Badge__container"><Badge /></div>
-                <div className="BadgeForm__container"><BadgesForm /></div>
+      return (
+        <div className="container">
+          <div className="Badge__container">
+            <Badge
+            firstName={this.state.form.firstName}
+            lastName={this.state.form.lastName}
+            email={this.state.form.email}
+            />
             </div>
-        )
+          <div className="BadgeForm__container">
+          <BadgesForm
+          formValues={this.state.form}
+          onChange={this.handleChange}            
+          />
+          </div>
+        </div>
+      )
     }
 }
 export default App;
