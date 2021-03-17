756debb191fa1c55ea5f204a643271f4cdff4909
freconur <freconur@gmail.com>
2021-03-16 23:59:04 -0500

    practica numero 4 de levantamiento de datos

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.css'
 import Badge from './components/Badge';
 import App from './pages/App';
+import Badges from './pages/Badges.js'
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badges />, container);
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,11 +3,33 @@ import Badge from '../components/Badge';
 import BadgesForm from '../components/BadgesForm';
 import './styles/App.css'
 class App extends React.Component {
+
+state = { form: {}}
+
+  handleChange = e => {
+		this.setState ({
+			form: {
+				...this.state.form,
+				[e.target.name] : e.target.value,
+			}
+		})
+	}
+
     render() {
         return (
             <div className="container">
-                <div className="Badge__container"><Badge /></div>
-                <div className="BadgeForm__container"><BadgesForm /></div>
+                <div className="Badge__container">
+								<Badge
+								firstName={this.state.form.firstName || 'Nombre'} 
+								lastName={this.state.form.lastName || 'Apellidos'}
+								email={this.state.form.email || 'email'} 
+								/></div>
+                <div className="BadgeForm__container">
+									<BadgesForm 
+									onChange={this.handleChange}
+									formValues={this.state.form}
+									/>
+									</div>
             </div>
         )
     }
